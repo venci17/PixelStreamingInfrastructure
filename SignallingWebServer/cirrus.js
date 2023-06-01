02d6277fcd00481aa546f9515077e2615ef46e8d
venci17 <g2b@abv.bg>
2023-06-01 14:35:51 +0300

    Added CrossDomain Embedding

--- SignallingWebServer/cirrus.js
+++ SignallingWebServer/cirrus.js
@@ -254,6 +254,15 @@ if (config.enableAuthentication) {
 }
 
 if(config.EnableWebserver) {
+	//Add Cross Domain Embedding
+	var allowCrossDomain = function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');	
+	res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
+	if (req.method === "OPTIONS") res.send(200);
+    	else next();
+	}
+	app.use(allowCrossDomain);
 	//Setup folders
 	app.use(express.static(path.join(__dirname, '/Public')))
 	app.use('/images', express.static(path.join(__dirname, './images')))
